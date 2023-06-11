983f03cb15cdc3614499a3b12e11683e4e7e4854
Milton Figueredo Miles <gratno@gmail.com>
2023-06-11 07:22:24 -0500

    Add Logger Factory and logger 4

---
+++ src/main/java/com/springboot/project/ProjectApplication.java
@@ -0,0 +1,25 @@
+package com.springboot.project;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(ProjectApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+
+
+		logger.debug("Mi mensaje debug");
+		logger.info("Mi mensaje info");
+		logger.warn("Mi mensaje warn");
+		logger.error("Mi mensaje error");
+
+	}
+
+}
