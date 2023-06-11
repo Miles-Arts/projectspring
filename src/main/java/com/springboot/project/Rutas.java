88ced48798b879751443459584ba6e933b9b6630
Milton Figueredo Miles <gratno@gmail.com>
2023-06-11 09:52:21 -0500

    Create formato JSON for Pruebas

--- src/main/java/com/springboot/project/Rutas.java
+++ src/main/java/com/springboot/project/Rutas.java
@@ -1,12 +1,16 @@
 package com.springboot.project;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class Rutas {
+
+
+    private final Logger logger = LoggerFactory.getLogger(Rutas.class);
 
     @GetMapping("/hola")
     String miPrimeraRuta() {
@@ -20,7 +24,18 @@ public class Rutas {
 
     @GetMapping("/libro2/{id}")
     String leerLibro2(@PathVariable int id, @RequestParam String params, @RequestParam String editorial) {
-        return "Libro: Programando con Java; Con ID: " + id + ", params " + params + ", editorial" + editorial;
+        return "Libro: Programando con Java; Con ID: " + id + ", params: " + params + ", editorial: " + editorial;
+    }
+
+    @PostMapping("/libro")
+    String guardarLibro(@RequestBody Map<String, Object> libro) {
+        libro.keySet().forEach(llave -> {
+
+
+            logger.debug("Llave {} valor {}", llave, libro.get(llave));
+        });
+
+        return "Libro guardado!!!";
     }
 
 }
